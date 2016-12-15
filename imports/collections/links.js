9e8197a23bd91cfdf3c4704fcab553c67dfdcf5d
nafiskarim <sahil.abrar@gmail.com>
2016-12-15 04:57:01 +0100

    url validation - package installed, logic written

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,8 +1,13 @@
 import { Mongo } from 'meteor/mongo'
+import valideUrl from 'valid-url'
+import { check, Match } from 'meteor/check'
 
 Meteor.methods({
   'links.insert': function (url) {
     console.log('attempting to save ', url);
+    // validation of url
+    valideUrl.isUri(url)
+    check(url, Match.Where(url => valideUrl.isUri(url)))
   }
 })
 
