ad35132d31970063daf0fdeb076839b926013fb9
nafis <nafis@883199.parkingcrew.net>
2016-12-15 03:49:28 +0100

    Header component added

--- client/main.js
+++ client/main.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
+import Header from './components/Header'
+
 const App = () => {
   return (
-    <h1>hello short link</h1>
+    <div>
+      <Header />
+      <h1>hello short link</h1>
+    </div>
   )
 }
 
